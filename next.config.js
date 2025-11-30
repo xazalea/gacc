d4e350c3a525764c9d8e883d77de9100ca770980
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-11-30 14:58:56 -0500

    Mark puppeteer-extra packages as externals

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+    serverComponentsExternalPackages: [
+      'puppeteer-core',
+      '@sparticuz/chromium-min',
+      'puppeteer-extra',
+      'puppeteer-extra-plugin',
+      'puppeteer-extra-plugin-stealth',
+      'clone-deep',
+      'merge-deep',
+    ],
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.externals = [...(config.externals || []), '@sparticuz/chromium'];
+      config.externals = [
+        ...(config.externals || []),
+        '@sparticuz/chromium-min',
+        'puppeteer-extra',
+        'puppeteer-extra-plugin',
+        'puppeteer-extra-plugin-stealth',
+        'clone-deep',
+        'merge-deep',
+      ];
       config.optimization = { ...config.optimization, minimize: true };
     }
     return config;
